121e64f88f40ae5ba56e3a6b835447927e2ed643
AssoulaimaniAsma <asma.assoulaimani@edu.uiz.ac.ma>
2026-02-09 22:33:46 +0100

    Push backend

---
+++ backend/gateway-service/src/test/java/com/assoulaimani/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assoulaimani.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ backend/user-service/src/test/java/com/assoulaimani/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assoulaimani.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
